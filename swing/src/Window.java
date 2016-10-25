9c707822145a3901429c8b0dc6236b87623fa60e
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2016-10-25 23:50:16 +0200

    Listener

--- swing/src/Window.java
+++ swing/src/Window.java
@@ -1,19 +1,58 @@
 import javax.swing.*;
 
+import java.awt.BorderLayout;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Window extends JFrame{ // fenetre principale
 
-  JButton button1 = null;
-  JButton button2 = null;
-  JButton buttonExit = null;
+  private static final long serialVersionUID = 1L;
+
+  private JPanel  panel;
+  private JButton button1;
+  private JButton button2;
+  private JButton buttonExit;
+  private JTextArea textArea;
 
   public Window(){
 
-    button1 = new JButton("SUPER BOUTON");
+    panel = new JPanel();
+
+    button1 = new JButton("Bouton 1");
+    button2 = new JButton("Bouton 2");
+
+    button1.addActionListener(new Button1Listner());
+    button2.addActionListener(new Button2Listner());
+
     add(button1);
+    add(button2);
 
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setTitle("Client Multimédia");
+    setSize(300, 300);
     pack();
     setVisible(true);
+  }
+
+  //Classes imbriquées
+  class Button1Listner implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+
+    }
+  }
+
+  class Button2Listner implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+      System.out.println("COUCOU");
+    }
+  }
+
+  class ButtonExitListner implements ActionListener{
+    public void actionPerformed(ActionEvent e){
+
+    }
   }
 }
