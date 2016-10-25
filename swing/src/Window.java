bf4f5c1a6a001e5dbc61566f3d052593dfa36975
Erwan CHERIAUX <erwan.cheriaux@gmail.com>
2016-10-26 00:40:32 +0200

    Mise en page de la fenetre

--- swing/src/Window.java
+++ swing/src/Window.java
@@ -23,36 +23,46 @@ public class Window extends JFrame{ // fenetre principale
 
     button1 = new JButton("Bouton 1");
     button2 = new JButton("Bouton 2");
+    buttonExit = new JButton("EXIT");
 
-    button1.addActionListener(new Button1Listner());
-    button2.addActionListener(new Button2Listner());
+    textArea = new JTextArea(5,5);
 
-    add(button1);
-    add(button2);
+    //button1.addActionListener(new Button1Listner());
+    //button2.addActionListener(new Button2Listner());
 
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setTitle("Client Multimédia");
-    setSize(300, 300);
-    pack();
+    setSize(300, 150);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+    //pack();
+    panel.setLayout(new GridLayout(1, 3));
+    //getContentPane().add(button1, BorderLayout.CENTER);
+    panel.add(button1);
+    panel.add(button2);
+    panel.add(buttonExit);
+
+    setLayout(new BorderLayout());
+    add(textArea, BorderLayout.CENTER);
+    add(panel, BorderLayout.SOUTH);
     setVisible(true);
   }
 
   //Classes imbriquées
   class Button1Listner implements ActionListener{
     public void actionPerformed(ActionEvent e){
-
+      System.out.println("Bouton 1 appuyé");
     }
   }
 
   class Button2Listner implements ActionListener{
     public void actionPerformed(ActionEvent e){
-      System.out.println("COUCOU");
+      System.out.println("Bouton 2 appuyé");
     }
   }
 
   class ButtonExitListner implements ActionListener{
     public void actionPerformed(ActionEvent e){
-
+      System.out.println("Bouton exit appuyé");
     }
   }
 }
